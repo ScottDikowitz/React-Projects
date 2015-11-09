24de946da5ba6c19c04f474262bd358130d9ae69
App Academy Student <appacademy@Apps-Mac-mini-13.local>
2015-11-09 11:39:30 -0500

    initial commit, complete autocomplete namesearch widget

---
+++ widgets/widgets.js
@@ -0,0 +1,43 @@
+var Widgets = React.createClass({
+  getInitialState: function () {
+    return {textInput: ""};
+  },
+
+  // pass in e to update, instead of document.getElementById
+  update: function(e) {
+    this.setState({textInput: e.currentTarget.value });
+  },
+
+
+  matches: function() {
+    var result = [];
+    this.props.names.forEach (function (el) {
+      var partName = el.slice(0, this.state.textInput.length);
+      if (partName === this.state.textInput) {
+        result.push(el);
+      }
+    }.bind(this));
+    return result;
+  },
+
+  handleClick: function(e) {
+    this.setState({textInput: e.currentTarget.innerText });
+    document.getElementById("nameInput").value = e.currentTarget.innerText;
+  },
+
+
+  render: function () {
+    // debugger;
+    //console.log(this.matches());
+    return <div>
+      <input id="nameInput" onChange={this.update}/>
+
+      <ul> {this.matches().map (function (el) {
+        return <li onClick={this.handleClick}>{el}</li>;
+        }.bind(this)) }
+      </ul>
+
+    </div>;
+  }
+  
+});
